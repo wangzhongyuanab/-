96c91197745e58e4ed949803450079cf7f0a025f
wzy <2275990862@qq.com>
2019-08-25 12:15:23 +0800

    企业权限管理系统

---
+++ ssm_dao/src/main/java/com/ssm/dao/SysLogDao.java
@@ -0,0 +1,26 @@
+package com.ssm.dao;
+
+import com.ssm.domain.SysLog;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface SysLogDao {
+
+
+    @Insert("insert into syslog(visitTime,username,ip,url,executionTime,method) values(#{visitTime},#{username},#{ip},#{url},#{executionTime},#{method})")
+    public void save(SysLog sysLog);
+
+    @Select("select * from syslog")
+    @Results({ @Result(id=true,column="id",property="id"),
+            @Result(column="visitTime",property="visitTime"),
+            @Result(column="ip",property="ip"),
+            @Result(column="url",property="url"),
+            @Result(column="executionTime",property="executionTime"),
+            @Result(column="method",property="method"),
+            @Result(column="username",property="username") })
+    List<SysLog> findAll();
+}
